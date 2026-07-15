4649f73774983d04171ee68d9eca73015eb19e8c
Cranchy <cranchydev@proton.me>
2026-07-15 23:37:52 +0100

    Enhance character input handling
    Added input actions for character movement and camera control.

--- YourCharacter.h
+++ YourCharacter.h
@@ -35,6 +35,15 @@ protected:
 	// Sets the bCameraReset to true
 	void SetCameraResetTrue(); 
 
+	// Rotates Camera 
+	void Look(const FInputActionValue& Value);
+
+	// Character Movement Functions (this is very archaic, please change this setup if possible)
+	void MoveForward();
+	void MoveLeft();
+	void MoveBackward();
+	void MoveRight();
+
 	/*
 	* Make sure that you're using either this bCameraReset or the one within the .cpp file.
 	* Remove the one that you're not using and keep the other one.
@@ -47,12 +56,49 @@ protected:
 	// UPROPERTY(BlueprintReadWrite, Category = "Camera")
 	// bool bCameraReset = false;
 
-	// If this IMC (InputMappingContext) isn't defaulted within the Editor, 
-	// you MUST instantiate it within the .cpp file.
-	UPROPERTY(EditAnywhere, Category = "Input")
-	UInputMappingContext* IMC_Default;
+	/*
+	* Adding extra variables here in combination with functions related to movement,
+	* to help you test the mechanic of CameraReset more easily.
+	* 
+	* The variables, therefore, are: 
+	* 1 - InputMappingContext
+	* 5 - InputActions
+	* 
+	*/
 
-	// Same goes here for this IA (InputAction).
+	// InputMappingContext 
 	UPROPERTY(EditAnywhere, Category = "Input")
-	UInputAction* IA_CameraReset;
+	UInputMappingContext* IMC_Default = nullptr;
+
+	// InputAction for CameraReset
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* IA_CameraReset = nullptr;
+
+	// InputAction for Forward Movement
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* IA_MoveForward = nullptr;
+
+	// InputAction for Left Movement
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* IA_MoveLeft = nullptr;
+
+	// InputAction for Backward Movement
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* IA_MoveBackward = nullptr;
+
+	// InputAction for Right Movement
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* IA_MoveRight = nullptr;
+
+	// InputAction for Camera Rotation
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* IA_Look = nullptr;
+
+	// Adds a Spring Arm to the Character that the CameraThirdPerson will be attached to
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Camera")
+	USpringArmComponent* SpringArm = nullptr;
+
+	// Adds a Camera Component to the Character so that the player can actually look around
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Camera")
+	UCameraComponent* Camera = nullptr;
 };
