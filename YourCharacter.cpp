950ce4691ac6675811ddb8ee6657eb3399bc718b
Cranchy <cranchydev@proton.me>
2026-07-15 23:31:16 +0100

    Implement character movement and input actions
    Added movement functionality and input actions for character control.

--- YourCharacter.cpp
+++ YourCharacter.cpp
@@ -1,14 +1,31 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "YourCharacter.h"
+
+#include "Components/CapsuleComponent.h"
 
 // Comment or remove this variable if you intend to use the one within .h
 bool bCameraReset = false;
 
 AYourCharacter::AYourCharacter()
 {
-	// . . .
+	// Create and Initialize Spring Arm
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->bUsePawnControlRotation = true;
+	SpringArm->SetRelativeLocation(FVector(0.0f, 0.0f, 100.f));
+
+	// Create and Initialize Camera
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+
+	// Makes CapsuleComponent visible to help understanding where the Character currently is
+	GetCapsuleComponent()->bHiddenInGame = false;
+
+	// Sets the Control Rotation to not affect this Actor
+	// This is a VERY important setup so that you can see the CameraReset in action
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	bUseControllerRotationYaw = false;
 }
 
 void AYourCharacter::BeginPlay()
@@ -46,7 +63,37 @@ void AYourCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompo
 				// NewObject here essentially creates an instance of this IMC (InputMappingContext) and assigns a memory address to it.
 				IMC_Default = NewObject<UInputMappingContext>(this, UInputMappingContext::StaticClass(), TEXT("IMC Default"));
 			}
-			
+
+			// Adds IA_Action if it wasn't previously setup from Blueprints
+			if (IA_CameraReset == nullptr)
+			{
+				IA_CameraReset = NewObject<UInputAction>(this, UInputAction::StaticClass(), TEXT("R"));
+			}
+
+			// Extraordinaly also creating two more InputAction to allow you to test this mechanic more laxxingly.
+			if (IA_MoveForward == nullptr)
+			{
+				IA_MoveForward = NewObject<UInputAction>(this, UInputAction::StaticClass(), TEXT("IA_MoveForward"));
+			}
+			if (IA_MoveLeft == nullptr)
+			{
+				IA_MoveLeft = NewObject<UInputAction>(this, UInputAction::StaticClass(), TEXT("IA_MoveLeft"));
+			}
+			if (IA_MoveBackward == nullptr)
+			{
+				IA_MoveBackward = NewObject<UInputAction>(this, UInputAction::StaticClass(), TEXT("IA_MoveBackward"));
+			}
+			if (IA_MoveRight == nullptr)
+			{
+				IA_MoveRight = NewObject<UInputAction>(this, UInputAction::StaticClass(), TEXT("IA_MoveRight"));
+			}
+
+			if (IA_Look == nullptr)
+			{
+				IA_Look = NewObject<UInputAction>(this, UInputAction::StaticClass(), TEXT("Look"));
+			}
+
+
 			if (UEnhancedInputLocalPlayerSubsystem* EILPS = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LP))
 			{
 				if (IMC_Default)
@@ -54,15 +101,9 @@ void AYourCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompo
 					// Sets the IMC_Default as the first Input Mapping Context.
 					EILPS->AddMappingContext(IMC_Default, 0);
 
-					// Adds IA_Action if it wasn't previously setup from Blueprints
-					if (IA_CameraReset == nullptr)
-					{
-						IA_CameraReset = NewObject<UInputAction>(this, UInputAction::StaticClass(), TEXT("R"));
-					}
-					
 					// Importantly verifies if the InputComponent (check InputComponent.h/.cpp for further information) is
 					// of the type 'EnhancedInputComponent', which is a more advanced version of the 'InputComponent'.
-					if (UEnhancedInputComponent* EIC = Cast<UEnhancedInputComponent>(InputComponent))
+					if (UEnhancedInputComponent* EIC = Cast<UEnhancedInputComponent>(PlayerInputComponent))
 					{
 						// Binds a key to a specific Input Action within the IMC.
 						// Note that EKeys could be any key on your keyboard, mouse, VR set, gamepad, phone, etc.
@@ -72,6 +113,41 @@ void AYourCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompo
 						// Lastly, sets the earlier InputAction (IA_CameraReset) within the Enhanced Input Component,
 						// with all the parameters defined above automatically.
 						EIC->BindAction(IA_CameraReset, ETriggerEvent::Started, this, &AYourCharacter::SetCameraResetTrue);
+
+						/*
+						* ------------------------
+						* Extra setup for Movement
+						* -------------------------
+						*/
+						IMC_Default->MapKey(IA_MoveForward, EKeys::W);
+						IMC_Default->MapKey(IA_MoveLeft, EKeys::A);
+						IMC_Default->MapKey(IA_MoveBackward, EKeys::S);
+						IMC_Default->MapKey(IA_MoveRight, EKeys::D);
+
+						EIC->BindAction(IA_MoveForward, ETriggerEvent::Triggered, this, &AYourCharacter::MoveForward);
+						EIC->BindAction(IA_MoveLeft, ETriggerEvent::Triggered, this, &AYourCharacter::MoveLeft);
+						EIC->BindAction(IA_MoveBackward, ETriggerEvent::Triggered, this, &AYourCharacter::MoveBackward);
+						EIC->BindAction(IA_MoveRight, ETriggerEvent::Triggered, this, &AYourCharacter::MoveRight);
+						/*
+						* -------------------------------
+						* Extra setup for Camera Rotation
+						* -------------------------------
+						*/
+
+						// Set ValueType as Axis2D
+						IA_Look->ValueType = EInputActionValueType::Axis2D;
+
+						FEnhancedActionKeyMapping& MapKeyLook = IMC_Default->MapKey(IA_Look, EKeys::Mouse2D);
+
+						UInputModifierNegate* NegateLook = NewObject<UInputModifierNegate>(IA_Look);
+
+						NegateLook->bX = false;
+						NegateLook->bY = true;
+						NegateLook->bZ = false;
+
+						MapKeyLook.Modifiers.AddUnique(NegateLook);
+
+						EIC->BindAction(IA_Look, ETriggerEvent::Triggered, this, &AYourCharacter::Look);
 					}
 				}
 			}
@@ -152,4 +228,44 @@ void AYourCharacter::CameraReset(float InDeltaTime)
 void AYourCharacter::SetCameraResetTrue()
 {
 	bCameraReset = true;
+}
+
+void AYourCharacter::MoveForward()
+{
+	// Find out which way is forward based on camera/controller
+	const FRotator Rotation = Controller->GetControlRotation();
+	const FRotator YawRotation(0.0f, Rotation.Yaw, 0.0f);
+
+	const FVector DirectionForward = GetActorForwardVector();
+	AddMovementInput(DirectionForward, 1.0f);
+}
+
+void AYourCharacter::MoveLeft()
+{
+	// Find out which way is forward based on camera/controller
+	const FRotator Rotation = Controller->GetControlRotation();
+	const FRotator YawRotation(0.0f, Rotation.Yaw, 0.0f);
+
+	const FVector DirectionLeft = GetActorRightVector();
+	AddMovementInput(DirectionLeft, -1.0f);
+}
+
+void AYourCharacter::MoveBackward()
+{
+	// Find out which way is forward based on camera/controller
+	const FRotator Rotation = Controller->GetControlRotation();
+	const FRotator YawRotation(0.0f, Rotation.Yaw, 0.0f);
+
+	const FVector DirectionBackward = GetActorForwardVector();
+	AddMovementInput(DirectionBackward, -1.0f);
+}
+
+void AYourCharacter::MoveRight()
+{
+	// Find out which way is forward based on camera/controller
+	const FRotator Rotation = Controller->GetControlRotation();
+	const FRotator YawRotation(0.0f, Rotation.Yaw, 0.0f);
+
+	const FVector DirectionRight = GetActorRightVector();
+	AddMovementInput(DirectionRight, 1.0f);
 }
