bff6afd9148075b687c2a8b33082fcf603f4762b
Cranchy <cranchydev@proton.me>
2026-07-17 14:10:52 +0100

    Refactor and renamed  "YourCharacter" to "MyCharacter"
    Refactor character class and input handling for improved functionality.
    
    Thanks for the extended description CoPilot!

--- YourCharacter.h
+++ YourCharacter.h
@@ -5,68 +5,63 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 
+// Remove comment if you're using SetupPlayerInputComponent here
+/**/
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
 #include "InputMappingContext.h"
 #include "InputAction.h"
 
-#include "YourCharacter.generated.h"
+// Adds Spring Arm and Camera components
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+#include "MyCharacter.generated.h"
 
 /**
- * 
- */
+*
+*/
 UCLASS()
-class YOURPROJECT_API AYourCharacter : public ACharacter
+class SAMPLEPROJECT_5_8_C_API AMyCharacter : public ACharacter
 {
 	GENERATED_BODY()
-	
+
+public:
+	// Sets default values for this character's properties
+	AMyCharacter();
+
 protected:
-	AYourCharacter(); // Constructor
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 
-	virtual void Tick(float DeltaTime) override; // Ticks every frame
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
 
-	virtual void BeginPlay() override; // Called after Class is spawned in the world
-
-	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override; // Usually called before BeginPlay() to set Player Input
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 	// Where the magic happens
-	void CameraReset(float InDeltaTime); 
+	void CameraReset(float InDeltaTime);
 
 	// Sets the bCameraReset to true
-	void SetCameraResetTrue(); 
+	void SetCameraResetTrue();
 
-	// Rotates Camera 
+	// Function used by a Timer later setup to forcefully end Camera Reset
+	void FinishCameraReset();
+
+	//Comment these out if you are using SetupPlayerInputComponent here
+
+	// Movement Function
+	void Move(const FInputActionValue& Value);
+
+	// Look Function (to rotate Camera)
 	void Look(const FInputActionValue& Value);
 
-	// Character Movement Functions (this is very archaic, please change this setup if possible)
-	void MoveForward();
-	void MoveLeft();
-	void MoveBackward();
-	void MoveRight();
+	// Jump Function (Yay! Jumping!)
+	void JumpStart(const FInputActionValue& Value);
 
-	/*
-	* Make sure that you're using either this bCameraReset or the one within the .cpp file.
-	* Remove the one that you're not using and keep the other one.
-	* Remember that, if you intend this variable to be accessed by another Class outside of this one,
-	* then having it here is the only option.
-	*
-	* Plus if you need/want it exposed to Blueprints, then keeping it here is also the correct option.
-	*
-	*/
-	// UPROPERTY(BlueprintReadWrite, Category = "Camera")
-	// bool bCameraReset = false;
-
-	/*
-	* Adding extra variables here in combination with functions related to movement,
-	* to help you test the mechanic of CameraReset more easily.
-	* 
-	* The variables, therefore, are: 
-	* 1 - InputMappingContext
-	* 5 - InputActions
-	* 
-	*/
-
-	// InputMappingContext 
+	// InputMappingContext that's going to be used for Player Input
 	UPROPERTY(EditAnywhere, Category = "Input")
 	UInputMappingContext* IMC_Default = nullptr;
 
@@ -74,31 +69,40 @@ protected:
 	UPROPERTY(EditAnywhere, Category = "Input")
 	UInputAction* IA_CameraReset = nullptr;
 
-	// InputAction for Forward Movement
+	// InputAction for Movement
 	UPROPERTY(EditAnywhere, Category = "Input")
-	UInputAction* IA_MoveForward = nullptr;
-
-	// InputAction for Left Movement
-	UPROPERTY(EditAnywhere, Category = "Input")
-	UInputAction* IA_MoveLeft = nullptr;
-
-	// InputAction for Backward Movement
-	UPROPERTY(EditAnywhere, Category = "Input")
-	UInputAction* IA_MoveBackward = nullptr;
-
-	// InputAction for Right Movement
-	UPROPERTY(EditAnywhere, Category = "Input")
-	UInputAction* IA_MoveRight = nullptr;
+	UInputAction* IA_Move = nullptr;
 
 	// InputAction for Camera Rotation
 	UPROPERTY(EditAnywhere, Category = "Input")
 	UInputAction* IA_Look = nullptr;
 
-	// Adds a Spring Arm to the Character that the CameraThirdPerson will be attached to
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Camera")
-	USpringArmComponent* SpringArm = nullptr;
+	// InputAction for Jump (because why not, I love Jumping() in games!)
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* IA_Jump = nullptr;
 
-	// Adds a Camera Component to the Character so that the player can actually look around
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Camera")
-	UCameraComponent* Camera = nullptr;
+	// Adding a Spring Component here for Camera usability
+	UPROPERTY(EditAnywhere, Category = "Camera")
+	USpringArmComponent* SpringArm;
+
+	// Adding a Camera here so that you can control the 
+	UPROPERTY(EditAnywhere, Category = "Camera")
+	UCameraComponent* Camera;
+
+	// The variable that allows the entirety of the Camera Reset mechanic to happen or not.
+	UPROPERTY(EditAnywhere, Category = "Camera|Reset")
+	bool bCameraReset = false;
+
+	// Used at the end when we want to SetControlRotation with RInterpTo.
+	UPROPERTY(EditAnywhere, Category = "Camera|Reset")
+	float InterpSpeed = 10.0f;
+
+	// Timer that runs whenever bCameraReset is called to true,
+	// that forcefully finishes the Camera Reset mechanic to prevent erratic behavior.
+	// Check implementation within MyPlayerController.cpp
+	FTimerHandle Timer_FinishCameraReset;
+
+	// Used on Timer_EndCameraReset
+	UPROPERTY(EditAnywhere, Category = "Camera|Reset")
+	float Timer_FCR_Seconds = 0.45f;
 };
