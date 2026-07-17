c67922d42d79ebe9bfcf57805cd1d6fc6dbd550f
Cranchy <cranchydev@proton.me>
2026-07-17 14:09:23 +0100

    Refactored and renamed
    Copy pasted from the original local file. Perhaps I'm getting too used to this? Oh, well. Sorry! Just saving time!

--- MyPlayerController.h
+++ MyPlayerController.h
@@ -15,25 +15,25 @@
 // Class generated file, crucial for this class to be functional.
 // Also part of the base #include but this MUST be the last 
 // otherwise the program won't compile.
-#include "YourPlayerController.generated.h"
+#include "MyPlayerController.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
-class SAMPLEPROJECT_5_8_C_API AYourPlayerController : public APlayerController
+class SAMPLEPROJECT_5_8_C_API AMyPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 
 protected:
 	// Default constructor
-	AYourPlayerController(); 
+	AMyPlayerController();
 
 	// Called after object from this class has spawned
-	virtual void BeginPlay() override; 
+	virtual void BeginPlay() override;
 
 	// Called every frame
-	virtual void Tick(float DeltaTime) override; 
+	virtual void Tick(float DeltaTime) override;
 
 	// Usually called before BeginPlay() to setup Player Input
 	virtual void SetupInputComponent() override;
@@ -43,6 +43,9 @@ protected:
 
 	// Sets the bCameraReset to true
 	void SetCameraResetTrue();
+
+	// Function used by a Timer later setup to forcefully end Camera Reset
+	void FinishCameraReset();
 
 	// Movement Function
 	void Move(const FInputActionValue& Value);
@@ -72,4 +75,20 @@ protected:
 	// InputAction for Jump (because why not, I love Jumping() in games!)
 	UPROPERTY(EditAnywhere, Category = "Input")
 	UInputAction* IA_Jump = nullptr;
+
+	// The variable that allows the entirety of the Camera Reset mechanic to happen or not.
+	UPROPERTY(EditAnywhere, Category = "Camera|Reset")
+	bool bCameraReset = false;
+
+	// Used at the end when we want to SetControlRotation with RInterpTo.
+	UPROPERTY(EditAnywhere, Category = "Camera|Reset")
+	float InterpSpeed = 10.0f;
+
+	// Timer that runs when called from the function bound to IA_CameraReset.
+	// Check implementation within MyPlayerController.cpp.
+	FTimerHandle Timer_FinishCameraReset;
+
+	// Used on Timer_EndCameraReset
+	UPROPERTY(EditAnywhere, Category = "Camera|Reset")
+	float Timer_FCR_Seconds = 0.45f;
 };
